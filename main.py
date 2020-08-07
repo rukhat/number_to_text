e2a79a8b83af6cceac28205470079414f882b15c
rukhat <himonthakur@gmail.com>
2020-08-07 14:03:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -19,16 +19,18 @@ digits = {
     }
 
 ten_plus = {
-        15: 'fifteen',
-        16: 'sixteen',
-        19: 'nineteen'
+        11: 'Eleven',
+        12: 'Twelve',
+        13: 'Thirteen',
+        14: 'Fourteen',
+        15: 'Fifteen',
+        16: 'Sixteen',
+        17: 'Seventeen',
+        18: 'Eighteen',
+        19: 'Nineteen'
     }
 
 tens = {
-        10: 'ten',
-        20: 'twenty',
-        30: 'thirty',
-        40: 'forty',
         10: 'Ten',
         20: 'Twenty',
         30: 'Thirty',
@@ -40,80 +42,64 @@ tens = {
         90: 'Ninety',
     }
 
-class Check:
-    def __init__(self, n):
-        self.n = n
-        
-    def single_digit(self):
-        print(digits[self.n].title(), end=" ")
-        
-    def tens(self):
-        print(tens[self.n].title(), end=" ")
-        
-    def hundred(self):
-        print("Hundred", end=" ")        
-    
-    def thousand(self):
-        print("Thousand", end=" ")
-    
-    def million(self):
-        print("Million", end=" ")
-    def billion(self):
-        print("Billion", end=" ")        
-
-
-
-def convert(n):
-    num = Check(n)
+def under_100(n):
     if n in range(0, 10):
-        num.single_digit()
-    elif n % 10 == 0:
-        if n % 100 != 0:
-            num.tens()
-        elif n % 1000 != 0:
-            n = n / 100
-            temp = Check(n)
-            temp.single_digit()
-            temp.hundred()
-        elif n % 10000 != 0:
-            n = n / 1000
-            temp = Check(n)
-            temp.single_digit()
-            temp.thousand()
-        elif n % 100000 != 0:
-            n = n / 1000
-            temp = Check(n)
-            temp.tens()
-            temp.thousand()
-        elif n % 1000000 != 0:
-            n = n / 100000
-            temp = Check(n)
-            temp.single_digit()
-            temp.hundred()
-            temp.thousand()
-        elif n % 10000000 != 0:
-            n = n / 1000000
-            temp = Check(n)
-            temp.single_digit()
-            temp.million()
-        elif n % 100000000 != 0:
-            n = n / 1000000
-            temp = Check(n)
-            temp.tens()
-            temp.million()
-        elif n % 1000000000 != 0:
-            n = n / 100000000
-            temp = Check(n)
-            temp.single_digit()
-            temp.hundred()
-            temp.million()
-        elif n % 10000000000 != 0:
-            n = n / 1000000000
-            temp = Check(n)
-            temp.single_digit()
-            temp.billion()
-            
-convert(2000000000)        
+        print(digits[n], end = " ")
+    elif n in range(10, 100, 10):
+        print(tens[n], end = " ")
+    elif n in range(11, 20):
+        print(ten_plus[n], end = " ")
+    else:
+        for i in range(2, 10):
+            if n in range((10*i+ 1), (10* (i + 1))):
+                d1 = (n // 10) * 10
+                d0 = n % 10
+                    
+                print(tens[d1], end = " ")
+                print(digits[d0], end = " ")
+
+def hundreds(n):
+    d2 = n // 100
+    d01 = n % 100
+        
+    print(digits[d2], end = " ")
+    print("Hundred", end = " ")
+    if d01:
+        print("and", end = " ")
+        under_100(d01)
+
+def thousands(n):
+    d3 = n // 1000
+    d012 = n % 1000
+        
+    print(digits[d3], end = " ")
+    print("Thousand", end = " ")
+    if d012:
+        hundreds(d012)        
+                
+def switch(n):
+    l = len(str(n))
+    
+    if l == 1 or l == 2:
+        under_100(n)
+    elif l == 3:
+        hundreds(n)
+    elif l == 4:
+        thousands(n)
+    
+                
+
+def main():
+    n = int(input("Enter a number:\n>> "))
+    
+    if n >= 0:
+        switch(n)
+    else:
+        print("Minus ")
+        switch(abs(n))
+                
+if __name__ == "__main__":
+    main()
         
         
         
